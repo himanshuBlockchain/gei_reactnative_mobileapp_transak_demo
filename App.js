8b63976720fd8cd02da60f4ad59ae3c740f93ec0
Himanshu Singh <himanshusingh@rapidinnovation.io>
2022-06-21 00:44:57 +0530

    Transak API Integration

--- App.js
+++ App.js
@@ -1,20 +1,89 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { NavigationContainer } from "@react-navigation/native";
+// import {  } from 'react-native';
+import { WebView } from "react-native-webview";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import { createDrawerNavigator } from "@react-navigation/drawer";
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
-export default function App() {
+const Drawer = createDrawerNavigator();
+const Tab = createBottomTabNavigator();
+
+import Transak from "./src/components/Transak";
+import Rapid from "./src/components/Rapid";
+// import Home from "./src/components/Home";
+import About from "./src/components/About";
+import Contact from "./src/components/Contact";
+import Follow from "./src/components/Follow";
+
+function DrawerNavigator() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <Drawer.Navigator>
+      <Drawer.Screen name="Buy/Sell Crypto using Transak" component={Transak} />
+      <Drawer.Screen name="About" component={TabNavigator} />
+      {/* <Drawer.Screen name="About" component={About} /> */}
+      <Drawer.Screen name="Contact" component={Contact} />
+      <Drawer.Screen name="Follow" component={Follow} />
+      {/* <Drawer.Screen name="Rapid" component={Rapid} /> */}
+    </Drawer.Navigator>
   );
 }
+
+function TabNavigator() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator>
+        <Tab.Screen name="Tab1" component={Tab1} />
+        <Tab.Screen name="Tab2" component={Tab2} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
+export default function App() {
+  return (
+    <>
+      <NavigationContainer>
+        {/* <Drawer.Navigator>
+          <Drawer.Screen name="Home" component={Transak} />
+          <Drawer.Screen name="About" component={About} />
+          <Drawer.Screen name="Contact" component={Contact} />
+          <Drawer.Screen name="Follow" component={Follow} />
+          <Drawer.Screen name="Rapid" component={Rapid} />
+        </Drawer.Navigator> */}
+        <Stack.Navigator>
+        <Stack.Screen name="Transak" component={DrawerNavigator} />
+        <Stack.Screen name="Rapid" component={Rapid} />
+      </Stack.Navigator>
+      </NavigationContainer>
+      {/* <Rapid/> */}
+      {/* <Transak /> */}
+    </>
+  );
+}
+function Tab1() {
+  return (
+    <>
+    <Text>Tab1</Text>
+    </>
+  );
+}
+function Tab2() {
+  return (
+    <>
+    <Text>Tab2</Text>
+    </>
+  );
+}
+
+const Stack = createNativeStackNavigator();
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    // flexDirection:'column',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
